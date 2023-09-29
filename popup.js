1dc8569d2a2fac86fbaa9857056dafc2842e2dff
Bruno Mello <bruno.barros.mello@gmail.com>
2023-09-29 14:23:42 -0300

    add icons

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 window.onload = function () {
-  chrome.storage.sync.get('storedUrls', function (data) {
+  chrome.storage.sync.get("storedUrls", function (data) {
     if (data.storedUrls) {
-      document.getElementById('urls').value = data.storedUrls.join('\n');
+      document.getElementById("urls").value = data.storedUrls.join("\n");
     }
   });
 };
 
-document.getElementById('openAndPin').addEventListener('click', function () {
-  const textareaValue = document.getElementById('urls').value;
-  const urls = textareaValue.split('\n');
+document.getElementById("openAndPin").addEventListener("click", function () {
+  const textareaValue = document.getElementById("urls").value;
+  const urls = textareaValue.split("\n");
 
-  chrome.storage.sync.set({ 'storedUrls': urls });
+  chrome.storage.sync.set({ storedUrls: urls });
 
   for (let url of urls) {
     // Check if the URL is valid.
@@ -26,9 +26,9 @@ document.getElementById('openAndPin').addEventListener('click', function () {
 // on close
 window.onbeforeunload = function () {
   // store the urls
-  const textareaValue = document.getElementById('urls').value;
-  chrome.storage.sync.set({ 'storedUrls': textareaValue.split('\n') });
-}
+  const textareaValue = document.getElementById("urls").value;
+  chrome.storage.sync.set({ storedUrls: textareaValue.split("\n") });
+};
 
 function isValidURL(string) {
   try {
